d246ec41322ed2b6272b8e6e372d756e06387f1b
Circ-Scribe <153608248+Circ-Scribe@users.noreply.github.com>
2023-12-12 16:21:42 +0300

    Update Calculator C program.c

--- Calculator C program.c
+++ Calculator C program.c
@@ -13,30 +13,21 @@ int main () {
     scanf(" %c", &operator);
     switch (operator) {
         case '+':
-        result = num1 + num2;
+        result = num1+num2;
         break;
-        // this is the next step 
         
         case '-': 
-        result = num1 - num2;
+        result = num1-num2;
         break;
         
         case '*': 
-        result = num1 * num2;
+        result = num1*num2;
         break;
         
         case '/': 
-        if (num2 != 0) {
-                result = (double)num1 / num2;
-            } else {
-                printf("Error! Division by zero is not allowed.\n");
-                return 1; // Exiting with an error code
-            }
+        result = num1/num2;
         break;
-        
-        default: 
-        printf("Invalid Operator!");
     }
-    printf("The result is: %0.2lf", result);
+    printf("The result: %lf", result);
     return 0; 
 }
